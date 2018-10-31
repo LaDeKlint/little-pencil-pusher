751ad34543cfe47f88e1141c37c2e77c416b250c
lars <lars@KlintsThinkpad>
2018-10-31 11:43:09 +0100

    Test Socket

--- src/littlepencilpusher/LittlePencilPusher.java
+++ src/littlepencilpusher/LittlePencilPusher.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package littlepencilpusher;
+
 import dk.sdu.mmmi.rd1.edgedetect.*;
+import dk.sdu.mmmi.rd1.robotcomm.RobotClient;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -27,12 +29,15 @@ public class LittlePencilPusher
         BufferedImage image = edge.getBufferedImage();
         File outputfile = new File("images/androidny.jpg");
         ImageIO.write(image, "jpg", outputfile);
-       
 
-        
-                
-  
-        
+//        RobotClient client = new RobotClient("123.123.123.123", 12345);
+//        
+//        if(!client.isConnected())
+//        {
+//            client.connect();            
+//        }
+//        client.write("Hej");
+//        client.disconnect(); 
     }
-    
+
 }
