9c4b99978ebd3c133d9ec6591c30ce5017405aee
LaDeKlint <44358360+LaDeKlint@users.noreply.github.com>
2018-11-26 09:02:47 +0100

    Add files via upload
    Nu kan vi sende til plc i små strenge - en for en.
    Ny TcpClient-klasse er tilføjet. Den er en kopi af RobotClient + mine ændringer, der er nødvendige for at lytte til plc'en.
    Jeg har også pillet i Peters kode for at undgå /n og Q-appends. Jeg ved ikke om det er det, der er galt, men det lader til at vi ikke får blyanten løftet mellem nogle af punkterne. Derfor bliver figurerne udfyldt, når vi tegner dem.

---
+++ src/littlepencilpusher/TcpClient.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package littlepencilpusher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lars
+ */
+public class TcpClient
+{   private String hostname;
+    private int port;
+    private Socket connection;
+    private PrintWriter out;
+    // tilføjet
+    private BufferedReader in;
+
+    /**
+     *
+     * @param hostname Hostname of the robot
+     * @param port Port of the robot
+     */
+    public TcpClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    /**
+     * Method which connects to the robot, using the parameters provided to the constructor.
+     */
+    public void connect() {
+        try {
+            connection = new Socket(hostname, port);
+            out = new PrintWriter(connection.getOutputStream(), true);
+            //tilføjet in-variabel der lytter på svar fra server
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(TcpClient.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error connecting to robot: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * This method is used to determine if a connection has been established to
+     * the robot.
+     *
+     * @return COnnection state to see if connection is established (true) or
+     * not (false).
+     */
+    public boolean isConnected() {
+        return connection.isConnected();
+    }
+    
+
+    /**
+     * This method writes a message to the robot iff a connection to the robot
+     * is established.
+     *
+     * @param message The message to write to the robot.
+     * @return 
+     * @throws java.io.IOException 
+     */
+    // tilføjet: returnerer svar fra server
+    public String write(String message) throws IOException
+    {
+        if (isConnected())
+        {
+            out.print(message);
+            out.flush();
+            String resp = in.readLine();
+            return resp;
+        } else
+        {
+            return "No respond!";
+        }
+    }
+
+    /**
+     * Method to close connection to the robot.
+     */
+    public void disconnect() {
+        if (isConnected()) {
+            try {
+                connection.close();
+            } catch (IOException ex) {
+                Logger.getLogger(TcpClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }  
+}
---
+++ src/littlepencilpusher/littlepencilpusher.java
@@ -0,0 +1,321 @@
+package littlepencilpusher;
+
+import dk.sdu.mmmi.rd1.edgedetect.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import javax.imageio.ImageIO;
+
+public class littlepencilpusher
+{ // start class
+
+    public static void main(String[] args) throws IOException
+    { // start main
+
+        /*
+        Sending image to the Edgedetector, and getting a bufferet image back, meaning a representation
+        og the edge we want to draw.
+        The buffered image is saved as a new image file.
+         */
+        EdgeDetector edge = new EdgeDetector("images/Android.jpg");
+        BufferedImage image = edge.getBufferedImage();
+        File outputfile = new File("images/AndroidNy.jpg");
+        ImageIO.write(image, "jpg", outputfile);
+
+        //String plcIP = "localhost";
+        String plcIP = "192.168.1.15";
+        int plcPort = 12345;
+        String plcOut = "X00000Y00000ZU";
+
+
+        /*
+        here we get an array of the edge of the image we can work with. It is a 2d int array of the 
+        magnitude of the image edge. The magnitude array method has been changed to 
+        return a binary truncate magnitude meaning it is eather "0" for black (draw) or "255" for white (don't draw)
+         */
+        int mag[][] = edge.getMagnitudeArray();
+
+        // for method description
+        printMagnitudeArray(mag);
+
+        plcOut = coordinates(mag);
+        System.out.println(plcOut.length());
+        sendToPlc(plcOut, plcIP, plcPort);
+
+    } // *******************end Main*******************************
+
+ 
+    // Her skrives data til plc.
+    public static void sendToPlc(String plcOut, String plcIP, int plcPort) throws IOException
+    {
+
+
+        TcpClient client = new TcpClient(plcIP, plcPort);
+        String resp = "";
+        // Hvor mange strenge skal vi sende?
+        int strLen = 9800;    //længden af de strenge vi skal sende
+        int strDiv = plcOut.length() / strLen; //antallet af hele strenge
+        System.out.println("Trying to send " + strDiv + " strings (plus remainder)");
+        String plcOutSubstr = "";
+
+        int divCount = 0;
+        while (divCount <= strDiv)
+        {
+            if (divCount < strDiv)
+            {
+                plcOutSubstr = plcOut.substring(divCount * strLen, divCount * strLen + strLen) + "Q";
+            } else if (divCount == strDiv)
+            {
+                plcOutSubstr = plcOut.substring(divCount * strLen) + "X00000Y00000ZUQ";  //her får vi den sidste rest med
+            }
+
+            System.out.println(divCount + ": " + plcOutSubstr);
+            client.connect();
+            if (!client.isConnected())
+            {
+                client.connect();
+            }
+            resp = client.write(plcOutSubstr);
+            client.disconnect();
+            System.out.println(resp);
+            if (resp.equals("OK"))
+            {
+                divCount++;
+            }
+
+        }
+
+    } //*********************end sendToPlc**********************
+
+    /*
+    This method prints the magnitude array values, for a grafical representation of the array.
+    It was made during the testfase, and does not carry any other funtionality 
+    than the test opotunity.
+    The print lines have been commented out, as they are not needed for the actual program.
+    The transpose method is also tested here, as an atempt to make an alternative to just
+    change the index output
+     */
+    public static void printMagnitudeArray(int n[][])
+    {// start method print
+
+        int m[][] = new int[n[0].length][n.length];
+
+        m = transpose(n);
+
+        for (int columnB = 0; columnB < m.length; columnB++)
+        { // start column for loop
+            for (int rowB = 0; rowB < m.length; rowB++)
+            { // start row for loop
+                //System.out.print(m[columnB][rowB] + "\t");
+
+            } // end row for loop
+            //System.out.println(" end column");
+        } // end column for loop
+
+    } // ********************end print method************************
+
+    /*
+     
+     */
+    public static String coordinates(int n[][])
+    {
+
+        StringBuilder result = new StringBuilder();
+
+        // check rows;
+        int m[][] = new int[n[0].length][n.length];
+
+        m = transpose(n);
+
+        int currentvalue;
+
+        int startIndex;
+
+        int counterLine = 0;
+
+        for (int i = 0; i < n.length; i++)
+        {
+
+            currentvalue = n[i][0];
+
+            startIndex = 0;
+            int index = i;
+            int counter = 0;
+            int track = 0;
+            int endIndex = 0;
+
+            for (int j = 0; j < n[0].length; j++)
+            {
+
+                // System.out.print(n[i][j] + "\t");
+                if (currentvalue != n[i][j])
+                {
+
+                    index = i;
+                    if (counter >= 1)
+                    {
+                        endIndex = j - 1;
+                    }
+
+                    /*    track = endIndex - startIndex;
+                    if (track != 0 ) {
+                        track += 1;
+                    }
+                    
+                     if (startIndex != 0 && track == 0)
+                    {
+                        track += 1;
+                    }
+                     */
+                    String x = String.format("%5s", i).replace(" ", "0");
+                    String z = null;
+                    String y = null;
+
+                    if (n[i][j] == 255)
+                    {
+                        y = String.format("%5s", j - 1).replace(" ", "0");
+                    } else
+                    {
+                        y = String.format("%5s", j).replace(" ", "0");
+                    }
+
+                    if (n[i][j] == 0)
+                    {
+                        z = "D";
+                    } else
+                    {
+                        z = "U";
+                    }
+
+                    result.append("X");
+                    result.append(x);
+                    result.append("Y");
+                    result.append(y);
+                    result.append("Z");
+                    result.append(z);
+
+                    startIndex = j;
+
+                    currentvalue = n[i][j];
+                    counter++;
+
+                }
+
+            }
+
+            counterLine++;
+//            if (counterLine == 15)
+//            {
+//                result.append("\n");
+//                counterLine = 0;
+//            }
+
+        }
+        for (int i = 0; i < n.length; i++)
+        {
+
+            currentvalue = n[0][i];
+            startIndex = 0;
+            int index = 0;
+            int counter = 0;
+            int track = 0;
+            int endIndex = 0;
+
+            for (int j = 0; j < n[0].length; j++)
+            {
+
+                if (currentvalue != n[j][i])
+                {
+
+                    index = j;
+                    if (counter >= 1)
+                    {
+                        endIndex = i - 1;
+                    }
+
+                    /*    track = endIndex - startIndex;
+                    if (track != 0 ) {
+                        track += 1;
+                    }
+                    
+                     if (startIndex != 0 && track == 0)
+                    {
+                        track += 1;
+                    }*/
+                    String y = String.format("%5s", i).replace(" ", "0");
+                    String z = null;
+                    String x = null;
+
+                    if (n[j][i] == 255)
+                    {
+                        x = String.format("%5s", j - 1).replace(" ", "0");
+                    } else
+                    {
+                        x = String.format("%5s", j).replace(" ", "0");
+                    }
+
+                    if (n[j][i] == 0)
+                    {
+                        z = "D";
+                    } else
+                    {
+                        z = "U";
+                    }
+
+                    result.append("X");
+                    result.append(x);
+                    result.append("Y");
+                    result.append(y);
+                    result.append("Z");
+                    result.append(z);
+
+                    startIndex = i;
+
+                    currentvalue = n[j][i];
+                    counter++;
+
+                }
+
+            }
+
+            counterLine++;
+//            if (counterLine == 15)
+//            {
+//                result.append("\n");
+//                counterLine = 0;
+//            }
+
+        }
+
+//        result.append("Q");
+        System.out.println(result);
+        return result.toString();
+
+    } // ***************** end method coordinates ********************
+
+    public static int[][] transpose(int m[][])
+    {
+        if (m == null || m.length == 0)
+        {
+            return m;
+        }
+
+        int width = m.length;
+        int height = m[0].length;
+
+        int[][] l = new int[height][width];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                l[j][i] = m[i][j];
+            }
+        }
+        return l;
+    }
+
+}
